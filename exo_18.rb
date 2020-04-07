2216c99341794eae1201e4e0e530524408bee0b7
Geoffrey2point0 <geoffrey.baratto@gmail.com>
2020-04-07 16:55:58 +0200

    correction exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -2,6 +2,6 @@ number = 01
 email = []
 while number <=50
   puts "jean.dupont#{number}@email.fr"
-  email << "jean.dupont#{number}&email.fr"
+  email << "jean.dupont#{number}@email.fr"
   number = number +1
 end
