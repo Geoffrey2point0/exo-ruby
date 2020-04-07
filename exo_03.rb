7da7a74ae343310b1705b4f6a924095ef6d422f0
Geoffrey2point0 <geoffrey.baratto@gmail.com>
2020-04-07 10:13:32 +0200

    ajout des 3 premiers exercices

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sey, ça donne : Bonjour, monde !"
+
+#le "#" devant une commande permet d'introduire un commentaire, il ne sera donc pas pris en considération dans le code.
