353bf40b9f49bce709690298a4b32d20f043df7e
Geoffrey2point0 <geoffrey.baratto@gmail.com>
2020-04-07 10:36:58 +0200

    ajout exo_04.rb

---
+++ exo_04.rb
@@ -0,0 +1,4 @@
+puts "Salut, ça farte ?
+
+#j'ai un message d'erreur m'indiquant qu'il y a un problème de chaïne de caractère sur la ligne 1
+# il manque les les guillemets à la fin de la ligne de code
