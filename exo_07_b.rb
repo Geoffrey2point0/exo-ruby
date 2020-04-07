df4805b507b54ebaba6ae0fd5cd77c75596019fc
Geoffrey2point0 <geoffrey.baratto@gmail.com>
2020-04-07 11:11:46 +0200

    ajout des exo 7

---
+++ exo_07_b.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp permet à l'utilisateur d'entrée lui même une valeur, dans notre cas "notre blase"
---
+++ exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp permet à l'utilisateur d'entrée lui même une valeur, dans notre cas "notre blase" et le curseur change de forme.
+#le programme a nous indique un message + la possibilité pour l'utilisateur d'écrire du texte
+#le programme b propose la même chose que le programme a, cependant un chevron apparaît à la ligne inférieur, ce qui permet d'être plus clair
+#le programme c ne permet que de rentrer du texte, aucunes indications pour l'utilisateur qu'on attends son Username.
